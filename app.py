898440cf7748049b8b5fd6edd0b7642e8f254c80
Ian Foster <foster@anl.gov>
2023-04-23 18:08:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import time
 
 app = Flask(__name__)
 
+initial_scale_factor = 0.1
 globus_url = "https://transfer.api.globus.org/v0.10/private/web_stats"
 """
 Example:
@@ -42,7 +43,7 @@ cache['last_time']     = int(time.time())
 cache['earlier_value'] = cache['last_value'] - 1
 cache['earlier_time']  = cache['last_time'] - 1
 cache['index']         = 0
-cache['scale_factor']  = 0.5
+cache['scale_factor']  = initial_scale_factor
 
 # If Globus web counter value hasn't changed, then estimate it to be
 #     <time since last reading> * <counter change rate>
@@ -79,11 +80,11 @@ def hello_world():
     elif this_value > last_value:
         cache['earlier_value'] = last_value
         cache['earlier_time']  = last_time
-        cache['scale_factor']  = 0.5
+        cache['scale_factor']  = initial_scale_factor
         print(f'{index}: Increase by {this_value-last_value} to {this_value}')
     else:  # this_value < last_value, which means that we increased by too much last time
+        print(f'{index}: Ahead by {last_value-this_value}, so increment by 1 to  {this_value+1}')
         this_value = last_value + 1
-        print(f'{index}: Ahead, so increment by 1 to  {this_value}')
 
     cache['last_value']    = this_value
     cache['last_time']     = this_time
